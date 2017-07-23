7db92ad38cb8179bc828db1300f2569490a0330c
Nenad Grgic <svrle@Grgas-MacBook-Pro.local>
2017-07-23 17:20:27 +0200

    postfixInstance master checkbox PostfixInstanceType

--- src/AppBundle/Form/PostfixInstanceType.php
+++ src/AppBundle/Form/PostfixInstanceType.php
@@ -18,6 +18,7 @@ class PostfixInstanceType extends AbstractType
             ->add('name')
             ->add('domains')
             ->add('isSingleInstance')
+            ->add('master')
             ->add('properties', CollectionType::class, array('entry_type' => PropertyType::class,
                 'by_reference' => false))
             ->add('save', SubmitType::class)
