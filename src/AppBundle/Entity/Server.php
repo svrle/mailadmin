4714c58953e48f8bd8f59323f84e847bb3365298
Nenad Grgic <svrle@Grgas-MacBook-Pro.local>
2017-09-21 22:17:26 +0200

    Server entity append properties

--- src/AppBundle/Entity/Server.php
+++ src/AppBundle/Entity/Server.php
@@ -28,6 +28,48 @@ class Server
      */
     protected $id;
 
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $ip;
+
+    /**
+     * @ORM\Column(type="string")
+     */
+    protected $name;
+
+    /**
+     * @return mixed
+     */
+    public function getIp()
+    {
+        return $this->ip;
+    }
+
+    /**
+     * @param mixed $ip
+     */
+    public function setIp($ip)
+    {
+        $this->ip = $ip;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * @param mixed $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
 
     /**
      * @return mixed
